d823d2d31b6a426336d0ddd5900e03219b62e631
jlopennweb <johnml@upenn.edu>
2024-07-01 13:35:12 -0400

    SimpleSAMLphp files for Drupal 10
    I copied the working SAML files from Penn Today 10 and changed a few things.
    The upstream SAML files from Penn Upstream Drupal 9 were not working out of the box.
    In particular it was looking for a LOGGER class that doesn't exist after upgrading to 10.
    These files may need to be changed if SAML doesn't work once Drupal 10 is up on Pantheon,
    but these files at least let us install simplesamlphp and build the site.

--- private/simplesamlphp/config/authsources.php
+++ private/simplesamlphp/config/authsources.php
@@ -7,34 +7,40 @@ if (isset($_SERVER['PANTHEON_ENVIRONMENT'])) {
   $siteid = "local";
 }
 
+// set entityID to primary live domain
+// $primary_domain = 'penntoday.upenn.edu';
+$primary_domain = $siteid;
+$entity_id = 'https://' . $primary_domain . '/simplesaml/module.php/saml/sp/metadata.php/default-sp';
+$sp_name = 'default-sp';
+
 $config = array(
 
-    // This is a authentication source which handles admin authentication.
-    'admin' => array(
-        // The default is to use core:AdminPassword, but it can be replaced with
-        // any authentication source.
+	// This is a authentication source which handles admin authentication.
+	'admin' => array(
+		// The default is to use core:AdminPassword, but it can be replaced with
+		// any authentication source.
 
-        'core:AdminPassword',
-    ),
-    // An authentication source which can authenticate against both SAML 2.0
-    // and Shibboleth 1.3 IdPs.
-    'default-sp' => array(
+		'core:AdminPassword',
+	),
+	// An authentication source which can authenticate against both SAML 2.0
+	// and Shibboleth 1.3 IdPs.
+	'default-sp' => array(
     'saml:SP',
-        'privatekey' => $siteid . '.pem',
-        'certificate' => $siteid . '.crt',
-        'entityID' => NULL,
-        'idp' => 'https://idp.pennkey.upenn.edu/idp/shibboleth',
-        'discoURL' => NULL,
-        'authproc' => array(
-            50 => array( // map attributes to names rather than numeric ids
-                'class' => 'core:AttributeMap', 'oid2name'
-            ),
-            60 => array( // replace colons in group names
-                'class'   => 'core:AttributeAlter',
-                'subject' => 'eduPersonEntitlement',
-                'pattern' => '/:/',
-                'replacement' => '.'
-            ),
-        ),
-    ),
+		'privatekey' => $sp_name . '.pem',
+		'certificate' => $sp_name . '.crt',
+		'entityID' => $entity_id,
+		'idp' => 'https://idp.pennkey.upenn.edu/idp/shibboleth',
+		'discoURL' => NULL,
+		'authproc' => array(
+			50 => array( // map attributes to names rather than numeric ids
+				'class' => 'core:AttributeMap', 'oid2name'
+			),
+			60 => array( // replace colons in group names
+				'class'   => 'core:AttributeAlter',
+				'subject' => 'eduPersonEntitlement',
+				'pattern' => '/:/',
+				'replacement' => '.'
+			),
+		),
+	),
 );
